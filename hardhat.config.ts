2bc67fdc520fc8d9746df88663ddad3e53ca6a07
GooGrand <sharkanovnikita@gmail.com>
2021-12-15 17:46:27 +0600

    upd compound and fixed tests migration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -334,6 +334,9 @@ const config: HardhatUserConfig = {
       },
       {
         version: "0.8.0"
+      },
+      {
+        version: "0.8.8"
       }
     ],
   },
--- test/BigBanger.test.ts
+++ test/BigBanger.test.ts
@@ -28,7 +28,7 @@ describe("MasterChef", function () {
 
     await this.sushi.transferOwnership(this.chef.address)
 
-    const sushi = await this.chef.sushi()
+    const sushi = await this.chef.relict()
     const devaddr = await this.chef.devaddr()
     const owner = await this.sushi.owner()
 
@@ -235,16 +235,16 @@ describe("MasterChef", function () {
       await advanceBlockTo("419")
       await this.chef.add("20", this.lp2.address, true)
       // Alice should have 10*1000 pending reward
-      expect(await this.chef.pendingSushi(0, this.alice.address)).to.equal("10000")
+      expect(await this.chef.pendingRelict(0, this.alice.address)).to.equal("10000")
       // Bob deposits 10 LP2s at block 425
       await advanceBlockTo("424")
       await this.chef.connect(this.bob).deposit(1, "5", { from: this.bob.address })
       // Alice should have 10000 + 5*1/3*1000 = 11666 pending reward
-      expect(await this.chef.pendingSushi(0, this.alice.address)).to.equal("11666")
+      expect(await this.chef.pendingRelict(0, this.alice.address)).to.equal("11666")
       await advanceBlockTo("430")
       // At block 430. Bob should get 5*2/3*1000 = 3333. Alice should get ~1666 more.
-      expect(await this.chef.pendingSushi(0, this.alice.address)).to.equal("13333")
-      expect(await this.chef.pendingSushi(1, this.bob.address)).to.equal("3333")
+      expect(await this.chef.pendingRelict(0, this.alice.address)).to.equal("13333")
+      expect(await this.chef.pendingRelict(1, this.bob.address)).to.equal("3333")
     })
 
     it("should stop giving bonus SUSHIs after the bonus period ends", async function () {
@@ -258,10 +258,10 @@ describe("MasterChef", function () {
       await this.chef.connect(this.alice).deposit(0, "10", { from: this.alice.address })
       // At block 605, she should have 1000*10 + 100*5 = 10500 pending.
       await advanceBlockTo("605")
-      expect(await this.chef.pendingSushi(0, this.alice.address)).to.equal("10500")
+      expect(await this.chef.pendingRelict(0, this.alice.address)).to.equal("10500")
       // At block 606, Alice withdraws all pending rewards and should get 10600.
       await this.chef.connect(this.alice).deposit(0, "0", { from: this.alice.address })
-      expect(await this.chef.pendingSushi(0, this.alice.address)).to.equal("0")
+      expect(await this.chef.pendingRelict(0, this.alice.address)).to.equal("0")
       expect(await this.sushi.balanceOf(this.alice.address)).to.equal("10600")
     })
   })
