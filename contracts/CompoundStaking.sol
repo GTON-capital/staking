82a51755ba7b15c7273dc4b6b170de1e0c3238da
GooGrand <sharkanovnikita@gmail.com>
2021-12-15 20:24:31 +0600

    cleaned contract

--- contracts/CompoundStaking.sol
+++ contracts/CompoundStaking.sol
@@ -1,41 +1,68 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.8;
+
 import "./interfaces/IERC20.sol";
 
 contract CompoundStaking {
+
+    address public owner;
+    bool public revertFlag;
+    uint public totalShares;
+    uint public tokenPerBlock;
+    uint public potentiallyMinted;
+    uint public lastRewardBlock;
+    uint public requiredBalance;
+
+    IERC20 public immutable token;
+
     struct UserInfo {
         uint share;
         uint tokenAtLastUserAction;
     }
 
-    IERC20 public immutable token;
     mapping(address => UserInfo) public userInfo;
-
-    uint256 public totalShares;
-    address public owner;
-    uint public tokenPerBlock;
-    uint public potentiallyMinted;
-    uint public lastRewardBlock;
-    uint public requiredBalance;
-    bool public revertFlag;
 
     constructor(
         IERC20 _token,
         uint _tokenPerBlock
     ) {
+        owner = msg.sender;
         token = _token;
         tokenPerBlock = _tokenPerBlock;
         lastRewardBlock = block.number;
     }
 
     modifier notReverted() {
-        require(!revertFlag, "owner: wut?");
+        require(!revertFlag, "Compound: reverted flag on.");
         _;
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "owner: wut?");
+        require(msg.sender == owner, "Compound: permitted to owner only.");
         _;
+    }
+
+    function setOwner(address _owner) public onlyOwner {
+        owner = _owner;
+        emit SetOwner(owner, _owner);
+    }
+
+    function toggleRevert() public onlyOwner {
+        revertFlag = !revertFlag;
+        emit RevertFlag(revertFlag);
+    }
+
+    function setTokenPerBlock(uint _tokenPerBlock) public onlyOwner {
+        updateRewardPool();
+        tokenPerBlock = _tokenPerBlock;
+    }
+
+    function withdrawToken(IERC20 _token, address _to, uint _amount) public onlyOwner {
+        require(_token.transfer(_to,_amount));
+    }
+
+    function pendingReward(address _user) public view returns(uint) {
+        return userInfo[_user].share * requiredBalance / totalShares;
     }
 
     function updateRewardPool() public {
@@ -46,23 +73,10 @@ contract CompoundStaking {
         lastRewardBlock = block.number;
     }
 
-    function setTokenPerBlock(uint _tokenPerBlock) public onlyOwner {
-        updateRewardPool();
-        tokenPerBlock = _tokenPerBlock;
-    }
-
-    function toggleRevert() public onlyOwner {
-        revertFlag = !revertFlag;
-    }
-
-    function withdrawToken(IERC20 _token, address _to, uint _amount) public onlyOwner {
-            require(_token.transfer(_to,_amount));
-    }
-
     function deposit(uint _amount) external {
         updateRewardPool();
-        require(_amount > 0, "Nothing to deposit");
-        require(token.transferFrom(msg.sender,address(this),_amount),"");
+        require(_amount > 0, "Compound: Nothing to deposit");
+        require(token.transferFrom(msg.sender,address(this),_amount),"Compound: insufficient approve");
         uint256 currentShares = 0;
         if (totalShares != 0) {
             currentShares = _amount * totalShares / requiredBalance;
@@ -75,25 +89,24 @@ contract CompoundStaking {
         requiredBalance += _amount;
     }
 
-    function withdrawAll() public {
-        withdraw(userInfo[msg.sender].share);
-    }
-
-    function pendingReward(address _user) public view returns(uint) {
-        return userInfo[_user].share * requiredBalance / totalShares;
-    }
-
     function withdraw(uint256 _share) public {
         updateRewardPool();
         UserInfo storage user = userInfo[msg.sender];
-        require(_share > 0, "Nothing to withdraw");
-        require(_share <= user.share, "Withdraw amount exceeds balance");
+        require(_share > 0, "Compound: Nothing to withdraw");
+        require(_share <= user.share, "Compound: Withdraw amount exceeds balance");
 
         uint256 currentAmount = requiredBalance * _share / totalShares;
         user.share = user.share - _share;
         totalShares = totalShares - _share;
 
         requiredBalance -= currentAmount;
-        require(token.transfer(msg.sender,currentAmount),"");
+        require(token.transfer(msg.sender,currentAmount),"Compound: Token transfer error.");
     }
+
+    function withdrawAll() public {
+        withdraw(userInfo[msg.sender].share);
+    }
+
+    event RevertFlag(bool flag);
+    event SetOwner(address oldOwner, address newOwner);
 }
