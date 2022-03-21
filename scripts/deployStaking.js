81a0de1890593e4174312287fff155fcd46aceb8
Alex Telegin <telegin.alexander@gmail.com>
2022-03-21 19:12:57 +0400

    Remove unused contracts, specify npm version in README

--- scripts/deployStaking.js
+++ scripts/deployStaking.js
@@ -9,7 +9,7 @@ async function main() {
     const Token = await ethers.getContractFactory("Token");
     const token = await Token.deploy();
 
-    console.log("Token address:", token.address);
+    console.log("Token address: ", token.address);
 }
 
 main()
